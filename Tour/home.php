15b4f655cebe29e17630e6dde58ed73dbb78eb26
Monarchfelix <138604460+Monarchfelix@users.noreply.github.com>
2023-12-01 20:56:22 +1100

    *Tour*
    My first Php tour work....

---
+++ Tour/home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tour</title>
+
+    <!--swiper css-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
+
+    <!--Font awesome style cdn js-->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css.css">
+
+      <!-- style -->
+      <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+
+<body>
+
+<!-- header section starts-->
+
+<section class="header">
+
+<a href="home.php" class="logo">travel.</a>
+</section>
+
+
+<!--header section starts-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <!-- swioper file-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+   <!-- custom js file-->
+<script src="js/script.js"></script>
+
+</body>
+</html>
